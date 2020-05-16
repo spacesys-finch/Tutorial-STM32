d8669f264cf0e5940cad82d113a9c66479ff18a6
rozariot <tobi.rozario@gmail.com>
2020-05-16 19:38:01 -0400

    made script to program through command line

---
+++ cmd_line_program.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Execute script with binary file argument to 
+# program stm32 through st link device
+# port settings currently set to SWD
+# can also change port to jtag by setting it as
+# port=JTAG
+
+# to run this on, open command prompt and run
+# bash test_cli.sh <hex_file>
+
+echo GO
+ST-LINK_CLI.exe -c SN=0049002C3137510B39383538 SWD -ME -P $1 -Run 0x08000000
