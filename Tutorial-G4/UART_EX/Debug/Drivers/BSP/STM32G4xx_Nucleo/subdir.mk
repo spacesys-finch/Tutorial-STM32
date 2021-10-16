8d87ff7c2f1b0551f7fa707b3b7aa77c214e37a5
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2021-10-16 18:13:12 -0400

    created UART_Example file for NUCLEO-H743ZI2 devkit

---
+++ Tutorial-G4/UART_EX/Debug/Drivers/BSP/STM32G4xx_Nucleo/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU ARM Embedded (7-2018-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.c 
+
+OBJS += \
+./Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32G4xx_Nucleo/stm32g4xx_nucleo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32G4xx_Nucleo/%.o: ../Drivers/BSP/STM32G4xx_Nucleo/%.c Drivers/BSP/STM32G4xx_Nucleo/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32G474xx -DDEBUG -c -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Include -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/BSP/STM32G4xx_Nucleo -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Core/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
--- comm_wrappers/readme.txt
+++
@@ -1,1 +0,0 @@
-Creating a set of wrappers on top of the HAL for the different STM32 communication protocools
